c80596681ac1c7d239c041431d03045a3d5287f6
Wiken2k3 <wiken2k3@gmail.com>
2024-04-26 21:42:03 +0700

    init project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster.lf4cncz.mongodb.net',
+    DB_USER: 'dangphuongdang1997',
+    DB_PASS: 'Phuongdang1997',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '315063330000', // in milliseconds
+    EMAIL_USER: 'dangphuongdang1997@gmail.com', // gmail service
+    EMAIL_PASS: 'rkposymhunjonhjc'
+  };
+  module.exports = MyConstants;
